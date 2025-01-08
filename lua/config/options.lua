332ecae76ca1eb4d2c978353d425ad6135f950de
Denis Andrei Szabo <byteme@byteme-1.home>
2025-01-08 22:30:29 +0100

    Added Lazy.nvim settings

---
+++ lua/config/options.lua
@@ -0,0 +1,11 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+vim.o.number = true
+vim.o.relativenumber = false
+vim.o.wrap = false
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.ignorecase = true
+vim.o.smartcase = true
